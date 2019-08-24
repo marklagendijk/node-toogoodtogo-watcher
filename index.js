af70b483680e5bd738ca64421099afc916c2eecc
Mark Lagendijk <mark@lagendijk.nl>
2019-08-24 16:08:36 +0200

    Implemented config editing

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 #!/usr/bin/env node
-const { flatMap } = require('rxjs/operators');
-const { initConfig } = require('./lib/config');
 const { pollFavoriteBusinesses } = require('./lib/poller');
 const { notifyIfChanged } = require('./lib/notifier');
+const { editConfig, resetConfig } = require('./lib/config');
 
-initConfig()
-    .pipe(flatMap(() => pollFavoriteBusinesses()))
-    .subscribe(businesses => notifyIfChanged(businesses), console.error);
+const argv = require('yargs')
+    .usage('Usage: toogoodtogo-watcher <command>')
+    .command('config', 'Edit the config file.', {
+        reset: {
+            type: 'boolean',
+            default: false
+        }
+    })
+    .command('watch', 'Watch your favourite busininesses for changes.')
+    .demandCommand()
+    .argv;
+
+switch(argv._[0]){
+    case 'config':
+        if(argv.reset){
+            resetConfig();
+        }
+        editConfig();
+        break;
+    case 'watch':
+        pollFavoriteBusinesses()
+            .subscribe(businesses => notifyIfChanged(businesses), console.error);
+        break;
+}
