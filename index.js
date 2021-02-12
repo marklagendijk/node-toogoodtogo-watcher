3e09ab19efe1fab2e71ce2ff4358aebf15fc1d63
darklamp <alessandro17.villa@mail.polimi.it>
2021-02-12 11:19:47 +0100

    Added configFile option to watch command, fetchable from .env too with prefil TOOGOODTOGO.

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 #!/usr/bin/env node
 const notifier = require("./lib/notifier");
 const { pollFavoriteBusinesses$ } = require("./lib/poller");
-const { editConfig, resetConfig, configPath } = require("./lib/config");
+const {
+  editConfig,
+  resetConfig,
+  configPath,
+  customConfig,
+} = require("./lib/config");
 
 const argv = require("yargs")
   .usage("Usage: toogoodtogo-watcher <command>")
   .command("config", "Edit the config file.")
   .command("config-reset", "Reset the config to the default values.")
   .command("config-path", "Show the path of the config file.")
-  .command("watch", "Watch your favourite busininesses for changes.")
+  .command("watch", "Watch your favourite businesses for changes.", (yargs) => {
+    return yargs.env("TOOGOODTOGO").option("configFile", {
+      alias: "cf",
+      usage: "Specify custom config file content.",
+    });
+  })
   .demandCommand().argv;
 
 switch (argv._[0]) {
@@ -25,6 +35,10 @@ switch (argv._[0]) {
     break;
 
   case "watch":
+    if (argv.configFile != null) {
+      customConfig(argv.configFile);
+    }
+
     pollFavoriteBusinesses$(notifier.hasListeners$()).subscribe(
       (businesses) => notifier.notifyIfChanged(businesses),
       console.error
